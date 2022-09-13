f82584f35f5e48e4d86578b5f87ee3799cfe8dec
Pedro Marques <ppmarq1@gmail.com>
2022-09-13 11:00:48 +0100

    Add controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'categories/index'
+  get 'payments/index'
+  get 'users/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
